1880d0e5ef60f775a4d5f0ab1be43565d6d41d94
unknown <yunus.emregunes@hotmail.com>
2023-03-02 23:32:02 +0300

    master update.

--- src/nlp/Nlp.js
+++ src/nlp/Nlp.js
@@ -1,71 +1,119 @@
 import React from 'react';
 import axios from 'axios';
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 
 
 
 function Nlp(props) {
-        const [str, setStr] = useState("");
-        const [countWord, setCountWord] = useState(0);
-        const [perWord, setPerWord] = useState(0);
+  const [str, setStr] = useState("");
+  const [countWord, setCountWord] = useState(0);
+  const [perWord, setPerWord] = useState(0);
+  const [resultList, setResultList] = useState([]);
 
-        const setString = (e) => {
-            setStr(e.target.value);
-        }
-        const baseURL = "https://jsonplaceholder.typicode.com/posts/1";
-        const [post, setPost] = useState(null);
-        useEffect(() => {
-            axios.get(baseURL).then((response) => {
-                setPost(response.data);
-            });
-        }, []);
-        if (!post) return null;
-
-        function countWords() {
-//Edge case: an empty array
-  if (str.length === 0) {
-    return {};
+  const setString = (e) => {
+    setStr(e.target.value);
   }
-  const output = {};
-  const perOut = {};
-  const strArr = str.split("")
-//A loop
-  let count = 0;
-  for (let i=0; i < strArr.length; i++) {
-    let word = strArr[i];
-    if (!word.includes(" ")) {
+
+  const baseURL = "https://jsonplaceholder.typicode.com/posts/1";
+  const [post, setPost] = useState(null);
+
+  /*useEffect(() => {
+    fetch('http://localhost:8000/questions').then(res => {
+      console.log("res", res);
+      return res.json
+    }).then(data => {
+      console.log("data", data);
+    })
+  }, []);*/
+
+  if (!post) return null;
+
+  function countWords() {
+
+    var resultArray = [];
+    //Edge case: an empty array
+    if (str.length === 0) {
+      return {};
+    }
+    const output = {};
+    const perOut = {};
+    const strArr = str.split("")
+
+    let count = 0;
+    for (let i = 0; i < strArr.length; i++) {
+      let word = strArr[i];
+      if (!word.includes(" ")) {
         if (output[word] === undefined) {
-            output[word] = 1;
-            count++;
+          output[word] = 1;
+          count++;
         } else {
-            output[word] += 1;
-            count += 1;
+          output[word] += 1;
+          count += 1;
         }
+      }
     }
-  }
     for (let j = 0; j < count; j++) {
-        let word = strArr[j];
-        if (!word.includes(" ")) {
-            let percentage = (output[word]/count) * 100;
-            perOut[word] = parseFloat(percentage.toFixed(2));
-        }
-    }
-    // setCountWord(output);
-    // setPerWord(perOut);
-  return [output, perOut];
-}
+      let word = strArr[j];
+      if (!word.includes(" ")) {
+        let percentage = (output[word] / count) * 100;
+        perOut[word] = parseFloat(percentage.toFixed(2));
+        var newObject = {
+          word: word,
+          count: output[word],
+          percentage: perOut[word]
+        };
 
-console.log(countWords());
-        return (
-            <div className="App">
-                <h1>Word Count</h1>
-            <p>Enter a sentence to count the words</p>
-            <input value={str} onChange={setString} type="text" id="input" />
-            <button onClick={countWords}>Count</button>
-                <p>{countWord}</p>
-                <p>{perWord}</p>
-            </div>
-        );
+        var resultArrayControl = false;
+        console.log(resultArray.length);
+        if (resultArray.length === 0) {
+          resultArray.push(newObject);
+        } else {
+          resultArray.forEach(element => {
+            if (element.word === newObject.word) {
+              resultArrayControl = true
+            }
+          });
+          if (!resultArrayControl) {
+            resultArray.push(newObject);
+          }
+        }
+      }
+    }
+    resultArray.forEach(element => {
+      setResultList(oldArray => [...oldArray, element]);
+    });
+
+    exportData(resultArray)
+    return [output, perOut];
+  }
+
+  //Export nlp datas to json file
+  const exportData = (resultArray) => {
+    const jsonString = `data:text/json;chatset=utf-8,${encodeURIComponent(
+      JSON.stringify(resultArray)
+    )}`;
+    const link = document.createElement("a");
+    link.href = jsonString;
+    link.download = "data.json";
+
+    //link.click();
+  };
+
+  return (
+    <div className="App">
+      <h1>Word Count</h1>
+      <p>Enter a sentence to count the words</p>
+      <input value={str} onChange={setString} type="text" id="input" />
+      <button onClick={countWords}>Count</button>
+      <div className='nlpContainer'>
+        <ul>{
+          resultList.map(item => <li key={item.word}>{item.word} === %{item.percentage}</li>)
+        }</ul>
+
+      </div>
+    </div>
+
+  );
 
 }
 
