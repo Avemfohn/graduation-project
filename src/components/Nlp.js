0b98d216f90e65f165d1fbba5f1d46fba16c656a
Mert Ercan <mertcan.erc@hotmail.com>
2023-03-07 18:03:23 +0300

    Database Edited.

--- src/components/Nlp.js
+++ src/components/Nlp.js
@@ -1,67 +1,84 @@
-import React from 'react';
-import axios from 'axios';
-import {useQuery} from "react-query";
-
+import React, { useState } from "react";
+import axios from "axios";
+import { useQuery } from "react-query";
 
 function Nlp(props) {
+  const [resultList, setResultList] = useState([]);
 
-    const fetchQuestions = async () => {
-        return axios.get("http://localhost:4000/questions");
+  const fetchQuestions = async () => {
+    return axios.get("http://localhost:4000/questions");
+  };
+  const { isLoading, error, data } = useQuery("questions", fetchQuestions);
+  if (isLoading) return <h2>Loading...</h2>;
+  if (error) return <h2>Error...</h2>;
 
+  //JavaScript code
+  function countWords(str) {
+    //Edge case: an empty array
+    const resultArray = [];
+    if (str.length === 0) {
+      return {};
     }
-  const {isLoading, error, data} = useQuery("questions", fetchQuestions)
-    if (isLoading) return <h2>Loading...</h2>;
-    if (error) return <h2>Error...</h2>;
-
-
-//JavaScript code
-function countWords(str) {
-//Edge case: an empty array
-  if (str.length === 0) {
-    return {};
-  } 
-  const output = {};
-  const perOut = {};
-  const strArr = str.split("")
-//A loop
-  let count = 0;
-  for (let i=0; i < strArr.length; i++) {
-    let word = strArr[i];
-    if (!word.includes(" ")) {
+    const output = {};
+    const perOut = {};
+    const strArr = str.split("");
+    let count = 0;
+    for (let i = 0; i < strArr.length; i++) {
+      let word = strArr[i].toLocaleLowerCase('tr');
+      if (!word.includes(" ")) {
         if (output[word] === undefined) {
-            output[word] = 1;
-            count++;
+          output[word] = 1;
+          count++;
         } else {
-            output[word] += 1;
-            count += 1;
+          output[word] += 1;
+          count += 1;
         }
+      }
     }
-  }
     for (let j = 0; j < count; j++) {
-        let word = strArr[j];
-        if (!word.includes(" ")) {
-            let percentage = (output[word]/count) * 100;
-            perOut[word] = parseFloat(percentage.toFixed(2));
-        }
-    }
-  return [output, perOut];
-}
+      let word = strArr[j].toLocaleLowerCase('tr');
+      if (!word.includes(" ")) {
+        let percentage = (output[word] / count) * 100;
+        perOut[word] = parseFloat(percentage.toFixed(2));
+        let newObject = {
+          word: word,
+          count: output[word],
+          percentage: perOut[word],
+        };
 
-let output = countWords('ask a bunch get a bunch');
-// { ask: 1, a: 2, bunch: 2, get: 1 }
-    
+        let resultArrayControl = false;
+        if (resultArray.length === 0) {
+          resultArray.push(newObject);
+        } else {
+          resultArray.forEach((element) => {
+            if (element.word === newObject.word) {
+              resultArrayControl = true;
+            }
+          });
+          if (!resultArrayControl) {
+            resultArray.push(newObject);
+          }
+        }
+      }
+    }
+
+    // resultArray.forEach(element => {
+    //   setResultList(oldArray => [...oldArray, element]);
+    //   console.log("element", element);
+    // });
+     console.log(resultArray);
+    return resultArray;
+  }
+
   /*
   const [countWord, setCountWord] = useState(0);
   const [perWord, setPerWord] = useState(0);
-
-
   const setString = (e) => {
     setStr(e.target.value);
-  }
 
+  }
   const baseURL = "https://jsonplaceholder.typicode.com/posts/1";
   const [post, setPost] = useState(null);
-
   /*useEffect(() => {
     fetch('http://localhost:8000/questions').then(res => {
       console.log("res", res);
@@ -70,7 +87,6 @@ let output = countWords('ask a bunch get a bunch');
       console.log("data", data);
     })
   }, []);
-
   if (!post) return null; */
 
   // function countWords() {
@@ -133,32 +149,28 @@ let output = countWords('ask a bunch get a bunch');
   // }
 
   //Export components datas to json file
- /* const exportData = (resultArray) => {
+  /* const exportData = (resultArray) => {
     const jsonString = `data:text/json;chatset=utf-8,${encodeURIComponent(
       JSON.stringify(resultArray)
     )}`;
     const link = document.createElement("a");
     link.href = jsonString;
     link.download = "data.json";
-
     //link.click();
   }; */
 
   return (
-
     <div className="App">
-      <div className='nlpContainer'>
+      <div className="nlpContainer">
         <ul>
-            {
-          data?.data.map((item) => {
-            return <li key={item.id}>{item.text}</li>
-          })
-        }</ul>
+          {data?.data.map((item) => {
+            //console.log("item", countWords(item.text));
+          })}
+          {countWords(data?.data[0].text)}
+        </ul>
       </div>
     </div>
-
   );
-
 }
 
 export default Nlp;
